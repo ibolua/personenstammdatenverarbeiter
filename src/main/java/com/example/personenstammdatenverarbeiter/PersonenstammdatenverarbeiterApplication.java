4ec036676d29fd1b80032ccb55ada2d4f4760c91
Ibrahim Ay <ibrahim.ay@veniture.net>
2024-01-17 16:56:07 +0100

    initial commit - backend

---
+++ src/main/java/com/example/personenstammdatenverarbeiter/PersonenstammdatenverarbeiterApplication.java
@@ -0,0 +1,13 @@
+package com.example.personenstammdatenverarbeiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonenstammdatenverarbeiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonenstammdatenverarbeiterApplication.class, args);
+	}
+
+}
